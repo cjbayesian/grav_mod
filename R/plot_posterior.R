4fcd49e95e1f367459a6b313a89104647561955d
Corey Chivers <corey.chivers@mail.mcgill.ca>
2012-07-23 16:31:37 -0400

    some key globals now definable in inits.ini

---
+++ R/plot_posterior.R
@@ -0,0 +1,72 @@
+
+d<-read.table('output/lib.mcmc')
+
+n_iter<-nrow(d)-1
+
+vars<-c("i",
+    "d",
+    "e",
+    "c",
+    "B_o",
+    "NAUT",
+    "KKUT",
+    "MGUT",
+    "CAUT",
+    "PPUT1",
+    "SIO3UR",
+    "DOC",
+    "COLTR",
+    "ALKTI",
+    "ALKT",
+    "PH",
+    "COND25",
+    "SECCHI.DEPTH")
+
+### Traces ###
+pdf('plots/traces.pdf')
+par(mfrow=c(2,1))
+for(i in 2:ncol(d))
+{
+   plot(d[,i],type='l',main=vars[i])
+   hist(d[2000:n_iter,i],main='')
+}
+dev.off()
+
+
+
+
+
+
+### Metaanalysis style (post mean and 95% BCI) ####
+e_x<-apply(d[2000:n_iter,],2,mean)
+q_x<-apply(d[2000:n_iter,],2,quantile,c(0.025,0.975))
+
+par(mar=c(5, 7.7, 2, 2))
+plot(e_x[6:18],6:18,
+   xlim=c(min(q_x[1,6:18]),max(q_x[2,6:18])  ),
+   ylim=c(5,19),
+   ylab='',
+   xlab='Posterior',
+   pch=20,
+   yaxt="n")
+
+y<-6:18
+axis(2, at=y,labels=vars[y], las=2)
+
+
+for(i in 6:18)
+{
+   arrows(e_x[i],i,q_x[1,i],i,angle=90,length=0.07)
+   arrows(e_x[i],i,q_x[2,i],i,angle=90,length=0.07)
+}
+
+abline(v=0,lty=2)
+
+
+### correlogram ###
+sub_index<-sample(2000:n_iter,1000)
+
+d_sub<-d[sub_index,2:18]
+plot(d_sub)
+
+
