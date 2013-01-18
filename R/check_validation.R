c768befbcffd7a847aca51531a09270d332523da
Corey Chivers <corey.chivers@mail.mcgill.ca>
2013-01-18 15:32:01 -0500

    Script to check the performance of the bootstrap model on the validation data

---
+++ R/check_validation.R
@@ -0,0 +1,29 @@
+
+lakes<-read.csv('../../2010_bytho_data/lakes_processed.csv',sep='\t',header=FALSE)
+pr<-read.csv('../output/pred_p.tab',sep='\t')
+
+pr<-as.matrix(pr)
+pr<-pr[,1:(ncol(pr)-1)]
+
+
+mean_pr<-apply(pr,2,mean)
+ord<-order(mean_pr)
+
+plot(pr[1,ord],pch=20)
+for(i in 1:nrow(pr))
+   points(pr[i,ord],pch=20,col=i)
+
+val_index<-as.matrix(read.csv('pred_p.tab',sep='\t',header=FALSE))
+val_index<-as.integer(val_index[1,1:(ncol(val_index)-1)])
+points(lakes[val_index[ord],4],pch=3)
+
+
+## -- ##
+## Inv prob as a function of lake size.
+plot(lakes[val_index[ord],1],pr[1,ord],col=lakes[val_index[ord],4]+1,pch=20)
+
+
+########### AUC #################
+source('~/AUC/AUC.R')
+AUC(d=lakes[val_index,4],pred=pr[3,],plot=TRUE)
+
